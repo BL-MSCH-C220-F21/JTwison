02e55beb10ed2583e0aae1613ad8abd2275fa8df
BL-MSCH-C220-F19 <53633703+BL-MSCH-C220-F19@users.noreply.github.com>
2022-01-05 22:32:07 -0500

    Initial commit

--- jtwison.js
+++ jtwison.js
@@ -1,3 +1,11 @@
+window.storyFormat({
+    "name": "JTwison",
+    "version": "0.0.1",
+    "author": "Jason Francis",
+    "description": "Export your Twine 2 story as a JSON document",
+    "proofing": false,
+    "source": "<html>\r\n  <head>\r\n    <title>{{STORY_NAME}}</title>\r\n    <script type=\"text/javascript\">\r\n      /**\r\n       * Twison - Twine 2 JSON Export Story Format\r\n       *\r\n       * Copyright (c) 2015 Em Walker\r\n       * https://lazerwalker.com\r\n       *\r\n       * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and\r\n       * associated documentation files (the \"Software\"), to deal in the Software without restriction,\r\n       * including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,\r\n       * and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so,\r\n       * subject to the following conditions:\r\n       *\r\n       * The above copyright notice and this permission notice shall be included in all copies or substantial\r\n       * portions of the Software.\r\n       *\r\n       * THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT\r\n       * LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.\r\n       * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,\r\n       * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE\r\n       * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.\r\n       */\r\n
+
 var JTwison = {
 
   /**
@@ -201,3 +209,5 @@ var JTwison = {
 }
 
 window.JTwison = JTwison;
+\r\n    </script>\r\n  </head>\r\n  <body>\r\n    <pre id=\"output\"></pre>\r\n    <div id=\"storyData\" style=\"display: none;\">\r\n      {{STORY_DATA}}\r\n    </div>\r\n    <script>\r\n      Twison.convert();\r\n    </script>\r\n  </body>\r\n</html>\r\n"
+});
